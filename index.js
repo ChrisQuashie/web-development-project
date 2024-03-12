81fc616f615878f32ac4d641a74708436444b8a7
Samuel <ekemasamuel954@gmail.com>
2024-03-11 20:02:58 -0400

    added clicked links to transition pages onclick

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const tabLinks = document.querySelectorAll(".tab-link");
+    const tabContents = document.querySelectorAll(".tab-content");
+
+    tabLinks.forEach(link => {
+        link.addEventListener("click", function() {
+            const tabId = this.getAttribute("data-tab");
+
+            tabLinks.forEach(link => link.classList.remove("active-tab"));
+            tabContents.forEach(content => content.classList.remove("active-tab"));
+
+            this.classList.add("active-tab");
+            document.getElementById(tabId).classList.add("active-tab");
+        });
+    });
+});
