c00b6eb348f73badb6b362a980b2264af964b378
sekema954 <sekema954@gmail.com>
2025-01-16 00:06:59 -0500

    edited vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   base: '/web-development-project/',
 });
