5dd8293ea6842a4689659e87262aa0e77b00691c
sekema954 <sekema954@gmail.com>
2025-01-16 00:52:10 -0500

    changed job pointer in experiences.html and logo for memberfist union

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,7 +1,7 @@
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import ksu from '../assets/images/ksu_logo.png'
-import credit_union from '../assets/images/credit_union_logo.png'
+import credit_union from '../assets/images/memberfirst_logo.webp'
 import bdo from '../assets/images/bdo_logo.png'
 function Experience() {
     const experiences = [
@@ -12,23 +12,26 @@ function Experience() {
             company_name: "BDO",
             date: "August 2024 - Present",
             points: [
-                "Validate and maintain internet access interfaces on remote site.",
-                "Administered HPE, Cisco ASA/FTD devices for seamingless network operations",
-                "Configure and optimized alerts in SolarWinds, Particularly for SDWAN circuit.",
-                "Analyzed TCP/IP and associated protocols to ensure optimized network performance."
+                "Led organization-wide port security project on network switch interfaces to enforce access restrictions based on MAC address authentication.",
+                "Configured and monitored Dedicated Internet Access IP addresses in SolarWinds using ICMP.",
+                "Executed DNS updates to enable accurate domain resolution and ensure seamless network connectivity for newly constructed sites.",
+                "Coordinated and executed hardware swaps and upgrades across remote branch locations, ensuring minimal downtime and seamless integration into existing network infrastructure.",
+                "Provisioned cloud networking solutions, including Azure VNet, gateway/NSG, and WAN/VNet peering, within Azure and AWS.",
+                "Designed and implemented network projects involving edge gateways, IDS/IPS, email filtering, and web content filtering solutions.",
+
             ]
         },
         {   
             id:2,
             icon:credit_union,
             title: "Network Admin",
-            company_name: "Credit Union",
+            company_name: "MembersFirst Credit Union",
             date: "May 2021- Aug 2023",
             points: [
-                "Configure routers, switches, and servers to ensure efficient and secure network.",
-                "Led IP phone system upgrade, planning and deploying VoIP infrastructure.",
-                "Engineered virtual networks for seamless VM and physical networks",
-                "Addressed firewal,l ACL, and VPN requests, providing technical support and solutions."
+                "Configure and maintain network devices such as routers, switches, and servers, ensuring that they operate efficiently and securely.",
+                "Led the IP phone system upgrade and migration project, including planning, deployment, and configuration of VoIP infrastructure.",
+                "Addressed incoming firewall, ACL, and VPN requests and provide customer troubleshooting and support.",
+                "Engineered and maintained virtual networks, ensuring seamless communication between virtual machines and the physical network infrastructure."
             ]
         },
     
@@ -39,10 +42,10 @@ function Experience() {
             company_name: "Kennesaw State University",
             date: "Sept 2023- Jul 2024",
             points: [
-                "Managed enterprice cisco ios configurations, implementing VLANS, VPN and PORT security.",
-                "Led lifecyle replacement project, deploying 800+ switches and 200+ switch stacks across campus network",
-                "Performed firmware upgrades on network switches.",
-                "Diagnosed WAN/LAN connectivity issues deploying solutions to minimize downtime."
+                "Troubleshooting and diagnosing WAN and LAN connectivity problems, employing effective solutions to minimize downtime and maintain network integrity.",
+                "Played a key role in the life cycle replacement project for KSU's enterprise network, overseeing the provisioning and deployment of over 800 switches and 200+ switch stacks..",
+                "Performed firmware updates on network switches to ensure optimal performance, security, and compatibility with the latest features and technologies.",
+                "Managed Cisco IOS in an enterprise-level network environment, configuring VLANs, port security protocols to, and establishing VPN connectivity to enhance secure communication within the network infrastructure."
             ]
         }
     ]
@@ -64,7 +67,7 @@ function Experience() {
                         <ul
                         className="list-disc"
                         key={idx}>
-                            <li>{points}</li>
+                            <li className="mb-2">{points}</li>
                         </ul>
                     ))}
                     <p className="text-[gray]">{data.date}</p>
